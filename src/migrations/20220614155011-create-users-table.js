2ee3354213163cc0990da4821bfbb6c6ca90ce78
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-06-14 12:52:33 -0300

    feat: generate create-users-table migration

---
+++ src/migrations/20220614155011-create-users-table.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      displayName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: Sequelize.STRING,
+      },
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
