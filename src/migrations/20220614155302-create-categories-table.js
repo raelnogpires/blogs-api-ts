5bfd01333ad8ce387f8c3aa4e9c17793987d7f35
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-06-14 12:53:38 -0300

    feat: generate create-categories-table migration

---
+++ src/migrations/20220614155302-create-categories-table.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Categories', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Categories');
+  }
+};
