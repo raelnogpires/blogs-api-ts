33795bca9247c08a080a5cf1db17bc7912ac18c6
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-06-14 12:38:39 -0300

    feat: create BlogPosts model

--- src/database/model/BlogPosts.ts
+++ src/database/model/BlogPosts.ts
@@ -2,3 +2,22 @@ import { DATE, INTEGER, Model, STRING } from 'sequelize/types';
 
 // @ts-ignore
 import db from '.';
+
+class BlogPosts extends Model {}
+
+BlogPosts.init(
+  {
+    title: STRING,
+    content: STRING,
+    userId: INTEGER,
+    published: DATE,
+    updated: DATE,
+  },
+  {
+    underscored: true,
+    sequelize: db,
+    timestamps: false,
+  },
+);
+
+export default BlogPosts;
