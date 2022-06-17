57b3b797dd592238a7ccc7e7799623e19b614d86
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-06-17 15:55:35 -0300

    feat: add getAllCategories to ICategoryRepository interface

--- src/@types/interfaces/category.repository.interface.ts
+++ src/@types/interfaces/category.repository.interface.ts
@@ -2,4 +2,5 @@ import { CategoryT } from '../types/category.type';
 
 export default interface ICategoryRepository {
   createCategory(name: string): Promise<CategoryT>,
+  getAllCategories(): Promise<CategoryT[]>,
 }
