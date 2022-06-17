b81b6d48dadb129e33317b42c341127212d47ec4
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-06-17 12:08:33 -0300

    feat: create getUserById method at UserService class

--- src/modules/user/service/user.services.ts
+++ src/modules/user/service/user.services.ts
@@ -1,4 +1,4 @@
-import { BadRequestError, ConflictError } from 'restify-errors';
+import { BadRequestError, ConflictError, NotFoundError } from 'restify-errors';
 import { UserReturnT, NewUserT, UserLoginT, UserT } from '../../../@types/types/user.type';
 
 import UserRepository from '../../../modules/repository/user.repository';
@@ -28,5 +28,14 @@ export default class UserService {
   public async getAllUsers(): Promise<UserReturnT[]> {
     const users = await this._repository.getAllUsers();
     return users;
+  }
+
+  public async getUserById(id: number): Promise<UserReturnT> {
+    const user = await this._repository.getUserById(id)
+    if (!user) {
+      throw new NotFoundError(`User doesn't exist`);
+    }
+
+    return user;
   }
 }
