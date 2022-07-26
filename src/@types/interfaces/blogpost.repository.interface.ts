f783b9f2dda5a558c3ac4773c14f9f513e419e8c
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-07-26 15:49:54 -0300

    feat: add new method do blogpost.repository.interface

--- src/@types/interfaces/blogpost.repository.interface.ts
+++ src/@types/interfaces/blogpost.repository.interface.ts
@@ -1,5 +1,6 @@
-import { CreatePostT, NewPostReturnT } from '../types/post.type';
+import { CreatePostT, NewPostReturnT, PostReturnT } from '../types/post.type';
 
 export default interface IBlogPostRepository {
   createPost(post: CreatePostT): Promise<NewPostReturnT>,
+  getAllPosts(): Promise<PostReturnT[]>,
 }
